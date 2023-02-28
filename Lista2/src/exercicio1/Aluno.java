b0683c4f5bd39ff20d35d1dcac538b3a1fde32a5
dusouza01 <eduardo.souza01@outlook.com>
2023-02-28 12:04:50 -0300

    aula 13/02

---
+++ Lista2/src/exercicio1/Aluno.java
@@ -0,0 +1,18 @@
+package exercicio1;
+
+
+public class Aluno {
+	// atributos ou propriedades ou variáveis de instancia
+	String nome;
+	int rm;
+	double n1, n2;
+
+	// método para calcular e retornar a média do aluno
+	public double total() {
+		double media;
+		media = (n1 + n2) / 2;
+		return media;
+
+	}
+
+}
