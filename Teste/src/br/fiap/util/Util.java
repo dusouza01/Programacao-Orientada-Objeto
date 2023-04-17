d37399e56ba16199ee9c358b46834e4dffc27eac
Eduardo <eduardo.souza01@outlook.com>
2023-04-17 20:59:46 -0300

    aula 17/04

--- Teste/src/br/fiap/util/Util.java
+++ Teste/src/br/fiap/util/Util.java
@@ -1,18 +1,77 @@
 package br.fiap.util;
 
+//ou coloca o nome do método ou o *
+
+import static javax.swing.JOptionPane.*;
+
+import br.fiap.carga.Carga;
+import br.fiap.cliente.Cliente;
+import br.fiap.viagem.Viagem;
+
+import static java.lang.Integer.*;
+
 public class Util {
+
+	private Viagem viagem = new Viagem();
+
 	public void menu() {
-		
+		int opcao;
+		// se o import nao for static, tem que colocar o Integer na frente do parseInt
+		do {
+			opcao = parseInt(showInputDialog(gerarMenu()));
+			if (opcao < 1 || opcao > 6) {
+				showMessageDialog(null, "Opção inválida");
+
+			} else {
+				switch (opcao) {
+				case 1:
+					reservar();
+					break;
+
+				case 3:
+					visualizar();
+					break;
+
+				}
+			}
+
+		} while (opcao != 6);
+
 	}
-	
+
+	private void visualizar() {
+		showMessageDialog(null, viagem.getDados());
+
+	}
+
+	private void reservar() {
+		String destino, nomeCliente;
+		int cnpj;
+
+		// toda janela grafica entra como string, estou transformando para int
+
+		cnpj = parseInt(showInputDialog("CNPJ"));
+		nomeCliente = showInputDialog("Cliente");
+		destino = showInputDialog("Destino");
+
+		Cliente cliente = new Cliente(cnpj, nomeCliente);
+		Carga carga = new Carga(destino, cliente);
+
+		if (viagem.reservar(carga)) {
+			showMessageDialog(null, "Carga reservada com sucesso");
+		} else {
+			showMessageDialog(null, "Chame o administrador");
+		}
+	}
+
 	private String gerarMenu() {
 		String aux = "Sistema de Reserva\n";
-		aux+= "1.Realizar reserva\n";
-		aux+= "2.Pesquisar reserva\n";
-		aux+= "3.Visualizar reservas\n";
-		aux+= "4.Capacidade reservada\n";
-		aux+= "5.Cancelar Reserva\n";
-		aux+= "6.Encerrar programa\n";
+		aux += "1.Realizar reserva\n";
+		aux += "2.Pesquisar reserva\n";
+		aux += "3.Visualizar reservas\n";
+		aux += "4.Capacidade reservada\n";
+		aux += "5.Cancelar Reserva\n";
+		aux += "6.Encerrar programa\n";
 		return aux;
 	}
 
--- Teste/src/br/fiap/viagem/Viagem.java
+++ Teste/src/br/fiap/viagem/Viagem.java
@@ -13,20 +13,18 @@ public class Viagem {
 		this.carga = new Carga[20];
 
 	}
-	
-	//metodo para reservar uma carga no navio
+
+	// metodo para reservar uma carga no navio
 	public boolean reservar(Carga carga) {
-		if(permitidoReservar(carga.getPeso())) {
+		if (permitidoReservar(carga.getPeso())) {
 			this.carga[index] = carga;
-			index ++;
+			index++;
 			return true;
-			
+
 		}
 		return false;
 	}
-	
-	
-	
+
 	// metodo para retornar a capacidade reservada
 
 	public double capacidadeReservada() {
@@ -38,16 +36,37 @@ public class Viagem {
 		return total;
 
 	}
-	
-	//metodo para verificar se é possivel fazer a reserva
+
+	// metodo para verificar se é possivel fazer a reserva
 	public boolean permitidoReservar(double peso) {
-		if(index < carga.length && capacidadeReservada()+peso <=capacidade ) {
+		if (index < carga.length && capacidadeReservada() + peso <= capacidade) {
 			return true;
 		}
 		return false;
-		
-		
 
+	}
+	// método para retornar os dados de todas as cargas reservadas
+
+	public String getDados() {
+		String aux = "";
+		for (int i = 0; i < index; i++) {
+			aux += carga[i].getDados() + "\n";
+
+		}
+		return aux;
+	}
+	//método para pesquisar uma carga pelo CNPJ
+	//tem relacao com o método cancelar
+	//vai retornar o índice do vetor
+	public int pesquisar(int cnpj) {
+		int aux = -1;
+		for(int i =0; i< index;i++) {
+			if(carga[i].getCliente().getCnpj() == cnpj) {
+				aux = i;
+			}
+		}
+		return aux;
+		
 	}
 
 }
